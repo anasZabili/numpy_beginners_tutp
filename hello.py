cf71edcf15b0af81349fd97e4e079bd5187d38ab
anas zabili <anaszab78@gmail.com>
2021-07-10 14:26:50 +0200

    final commit

--- hello.py
+++ hello.py
@@ -69,34 +69,115 @@ arrayZero2d = np.zeros((2,3))
 print(arrayZero2d)
 
 arrayZero3d = np.zeros((3, 4, 2))
-print("\n", arrayZero3d)
+print("\n fill with zero 3d", arrayZero3d)
 
 #initializing with one
 arrayOne3d = np.ones((3, 3, 3), dtype="int16")
-print("\n", arrayOne3d)
+print("\n fill with one 3d ", arrayOne3d)
 
 #initializing with x
 arrayX3d = np.full((2,2,2), 4, dtype="int16")
-print("\n", arrayX3d)
+print("\n fixed number 3d", arrayX3d)
 
 #initializing with random float numbers (pretty usefull)
 arrayRandom3d = np.random.rand(2, 2, 2)
-print("\n", arrayRandom3d)
+print("\n random number int", arrayRandom3d)
 
 #initializing with random int number
 
 arrayRandomInt3d = np.random.randint(low=10, size=(3,3))
-print("\n", arrayRandomInt3d)
+print("\n fill a matrix with random numbers", arrayRandomInt3d)
 
 #repeat an array
 arrayToRepeat = np.array([[1, 2, 3]])
 arrayRepeated = np.repeat(arrayToRepeat, 3, axis=0)
-print("\n", arrayRepeated)
+print("\n repeated array", arrayRepeated)
 
 # exercice 31:40
 
 exerciceMatrix = np.ones((5,5), dtype="int16")
 exerciceMatrix[1:-1, 1:-1] = np.zeros((3, 3))
 exerciceMatrix[2, 2] = 9
-print("\n",exerciceMatrix)
+print("\n exercice matrix",exerciceMatrix)
 
+# copy array
+
+a = np.array([1, 2, 3])
+b = a.copy()
+
+
+# Mathematics
+
+# all the classique mathe operation works with arrays
+
+addArray = np.array([[1, 2, 3]]) + 6
+print("\n add constant to an array", addArray)
+
+# trigo operarion
+
+sinArray = np.array([1, 2, 3])
+print("\n sinus of array", np.sin(sinArray))
+
+# Linear algebra
+# mutiplie matrix
+a = np.ones((2, 3))
+b = np.full((3, 2), 2)
+print("\n matrix multiplication", np.matmul(a, b))
+
+# Determinante of a matrix
+c = np.identity(3)
+print("\n determinant", np.linalg.det(c))
+
+#Statistics
+
+stats = np.array([[4, 5, 6], [7, 8, -1]])
+# axis=0 coupe vertical du tableau
+# axis=1 coupe horizontal du tableau
+print("\n min stats", np.min(stats, axis=0))
+print("\n max stats", np.max(stats, axis=1))
+
+
+# Reorganizing arrays
+
+# reshape array
+before = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+# when performing a reshape the new structure need to have the same number of elements
+print("\n reshape array", before.reshape((2, 2, 2)))
+
+# vertically stacking vectore
+v1 = np.array([1, 2, 3, 4])
+v2 = np.array([5, 6, 7, 8])
+
+print("\n merge of two array stacked vertically to create a matrix\n", np.vstack([v1, v2]))
+
+# horizontal stack (need to have the same number of rows)
+vh1 = np.zeros((2, 2))
+vh2 = np.ones((2, 4))
+
+print("\n merge of two array stacked horizontal to create a matrix\n", np.hstack([vh1, vh2]))
+
+
+# Load date from file
+
+fromFileArray = np.genfromtxt("data.txt", delimiter=",", dtype="int16")
+# converte the generated array to  other types
+convertedArray = fromFileArray.astype("float32")
+print("\n array generated from textFile", fromFileArray)
+
+
+# Boolean masking and advanced dindexing (very usefull)
+# we can use this concept to apply a condition on each of our element
+
+print("\n apply boolean mask on each element", fromFileArray > 50)
+
+
+# Get only the element who passed the mask (usefull aswell )
+
+print("\n print only the element who respect the condition", fromFileArray[fromFileArray > 50])
+
+# Check if any of the value of the column are greater than 50
+
+print("\n  Check if any of the value of the column are greater than 50", np.any(fromFileArray > 50, axis=0))
+print("\n  Check if all of the value of the column are greater than 50", np.all(fromFileArray > 50, axis=0))
+
+# boolean operator 
